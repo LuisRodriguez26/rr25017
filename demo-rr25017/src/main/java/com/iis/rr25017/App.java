5a9d37933905381a83b1932876f3baea8369769b
Luis Mario <rr25017@ues.edu.sv>
2025-05-11 01:40:35 -0600

    My first commit

---
+++ demo-rr25017/src/main/java/com/iis/rr25017/App.java
@@ -0,0 +1,17 @@
+package com.iis.rr25017;
+
+/**
+ * Hello world!
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main(String[] args) 
+    {
+    	App hw = new App() ;
+        System.out.println(hw.Hello() );
+    }
+}
